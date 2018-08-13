a3e1e9ffa0040ad0954f58375b062025ccd7178f
AlexDuOl <contactogatovagabundo@gmail.com>
2018-08-13 13:42:52 -0500

    Subiendo ajustes

--- js/app.js
+++ js/app.js
@@ -2,6 +2,35 @@ $(document).ready(function() {
     $('.collapsible').collapsible(); 
 });
 
+//*****MOSTRAR******//
+
+$("#movies").click(function(collapsibleMovies){
+    $(".collapsible").addClass("show-on-medium-and-up"); 
+    console.log(collapsibleMovies);
+      
+});
+
+
+//*****TEMPLATE******/
+function collapsibleMovies(title, year, actors, plot, awards, director, website, poster) {
+   
+    const collapsibleMovies =
+    `<li>
+        <div class="row collapsible-header">
+            <img class="col l2" src="${poster}"/>
+            <p class="col l5 texto">${title}</p>          
+            <p class="col l2 texto">Año: ${year}</p>
+            <p class="col l3 texto">Director: ${director}</p> 
+        </div>
+        <div class="collapsible-body texto"><span>Actores: ${actors}</span></div>
+        <div class="collapsible-body texto"><span>Trama: ${plot}</span></div>
+        <div class="collapsible-body texto"><span>Premios: ${awards}</span></div>
+        <div class="collapsible-body texto"><span>Sitio Web: ${website}</span></div>
+    </li>`
+
+    return collapsibleMovies
+}
+
 
 //*****OBTENIENDO DATA PARA TEMPLATE******//
 $(document).ready(function() {
@@ -23,39 +52,16 @@ $(document).ready(function() {
         let awards = data.Awards;
         let director = data.Director;
         let website = data.Website;
+        let poster = data.Poster;
 
 
-        $(".collapsible").append(collapsibleMovies(title, year, actors, plot, awards, director, website));
+        $(".collapsible").append(collapsibleMovies(title, year, actors, plot, awards, director, website, poster));
 
     })
     
 })
 
-//*****MOSTRAR******//
 
-$("#movies").click(function(){
-    $(".collapsible").removeClass(".hiden").addClass(".show-on-large");   
-});
-
-
-//*****TEMPLATE******//
-function collapsibleMovies(title, year, actors, plot, awards, director, website) {
-   
-    const collapsibleMovies =
-    `<li>
-        <div class="row collapsible-header">
-            <p class="col l4">${title}</p>          
-            <p class="col l4">Año: ${year}</p>
-            <p class="col l4">Director: ${director}</p>                
-        </div>
-        <div class="collapsible-body"><span>Actores: ${actors}</span></div>
-        <div class="collapsible-body"><span>Trama: ${plot}</span></div>
-        <div class="collapsible-body"><span>Premios: ${awards}</span></div>
-        <div class="collapsible-body"><span>Sitio Web: ${website}</span></div>
-    </li>`
-
-    return collapsibleMovies
-}
 
 
 
